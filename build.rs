c9ec55ef6b3f14586490fc283a0b3dd05591b162
Tyr Chen <tyr.chen@gmail.com>
2021-03-18 19:27:28 -0700

    fix doc build issue

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
+use std::option_env;
+
 fn main() {
+    let build_enabled = option_env!("BUILD_PROTO")
+        .map(|v| v == "1")
+        .unwrap_or(false);
+
+    if !build_enabled {
+        println!("=== Skipped compiling protos ===");
+        return;
+    }
+
+    println!("Build...");
     tonic_build::configure()
         .out_dir("src/fixtures")
         .compile(&["examples/hello.proto"], &["examples"])
